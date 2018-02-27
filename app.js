c7a442d572c8850a06ed21aaa4f8d5ba5029b539
JoshEdgell <josh.0.edgell@gmail.com>
2018-02-26 21:20:30 -0500

    delete method works

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const app = angular.module('OfficeTrivia', []);
 
 app.controller('MainController', ['$http', function($http){
-  this.url = 'http://localhost:12345';
+  const controller = this;
+  this.url = 'http://localhost:12345/questions/';
+  this.currentQuestion = {};
   this.getQuestionIds = function(){
     $http({
       method: 'GET',
-      url: this.url + '/questions/'
+      url: this.url
     }).then(function(response){
-      console.log(response, 'error');
+      console.log(response, 'resposne');
     }, function(error){
-      console.log(error, 'response')
+      console.log(error, 'error')
     })
   };
-  this.getQuestionIds();
+  this.getRandomQuestion = function(){
+    $http({
+      method: 'GET',
+      url: this.url + 'random'
+    }).then(function(response){
+      controller.currentQuestion = response.data;
+      console.log(controller.currentQuestion, 'current question');
+    }, function(error){
+      console.log(error, 'random question error');
+    })
+  };
+  this.deleteQuestion = function(id){
+    $http({
+      method: 'DELETE',
+      url: this.url + id
+    }).then(function(response){
+      console.log(response, 'response from delete method');
+    }, function(error){
+      console.log(error);
+    })
+  }
+  this.getRandomQuestion();
 }])
