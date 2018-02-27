a6bc12a54fa39eee63cabed679ecbc4dc9748121
JoshEdgell <josh.0.edgell@gmail.com>
2018-02-26 20:51:05 -0500

    can call backend for question ids

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const app = angular.module('OfficeTrivia', []);
 
 app.controller('MainController', ['$http', function($http){
-  this.word = 'poop'
+  this.url = 'http://localhost:12345';
+  this.getQuestionIds = function(){
+    $http({
+      method: 'GET',
+      url: this.url + '/questions/'
+    }).then(function(response){
+      console.log(response, 'error');
+    }, function(error){
+      console.log(error, 'response')
+    })
+  };
+  this.getQuestionIds();
 }])
